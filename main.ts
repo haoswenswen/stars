df3ce4a78d82ff5ff416c108cf2875696e770b7e
haoswenswen <114084940+haoswenswen@users.noreply.github.com>
2022-09-21 21:54:29 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,52 @@
+basic.forever(function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+    basic.showString("HI")
+    basic.showIcon(IconNames.Target)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
